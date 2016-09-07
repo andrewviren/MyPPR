6e71a374ab466adcac0e20f5317f3d2bbd3abadd
Andrew Viren <andrewviren@users.noreply.github.com>
2016-09-06 22:15:43 -0400

    last week

--- ppr.js
+++ ppr.js
@@ -54,7 +54,7 @@ window.KnackInit = function($) {
       daysSinceMonday = today.getDay() - 1;  // 0 is Sunday, 1 is Monday
       daysSinceMonday = (daysSinceMonday + 7) % 7;  // Correct for negatives
       daysSinceMonday = daysSinceMonday || 7;  // Correct to last Monday if today is Monday
-      monday.setDate(today.getDate() - daysSinceMonday);
+      monday.setDate(today.getDate() - daysSinceMonday - 7);
       $attendanceWeekField.val((monday.getMonth() + 1) + '/' + monday.getDate() + '/' + monday.getFullYear());
     }
 
