aa4d7e0012e789f53b8603b6e054814b3a47b2c8
Mohamed Atia <Mohamed.Atia@phila.gov>
2019-06-11 11:31:24 -0400

    added js changes, markdowns for new features

--- ppr.js
+++ ppr.js
@@ -1,31 +1,34 @@
 var PPR = PPR || {};
 
+
 window.KnackInit = function($) {
   'use strict';
 
   /*
-  Page "scene_93": Program Program Attendance form and table
+  Page "scene_383": Program Program Attendance form
   ========================================================
   Views
   -----
   - view_154: Weekly Program Attendance form
   Fields
   ------
-  - field_151: Number of Registered Participants
+  - field_151: Number of Registered Participants 
   - field_150: Number of Unique Participants
   - field_152: Number of Spectators
   - field_148: Attendance Week
+  - field_380: Facility Name
   */
+  PPR.attendanceForm = PPR.attendanceForm || {};
 
-  $(document).on('knack-page-render.scene_234', function(event, page) {
-    PPR.attendanceForm = PPR.attendanceForm || {};
+  $(document).on('knack-page-render.scene_362', function(event, page) {
 
     var numericFields = [
       '#field_151',
       '#field_150',
       '#field_152'
     ];
-    var $attendanceWeekField = $('#view_426-field_148');
+    var $attendanceWeekField = $('#view_661-field_148');
+    var $facilityField = $('#view_661-field_380');
     var today, monday, daysSinceMonday;
      
       
@@ -34,17 +37,31 @@ window.KnackInit = function($) {
     /***********************************/
     $attendanceWeekField.datepicker('option', {
       beforeShowDay: function (date) {
-        //console.debug(date);
+        console.debug(date);
         return [date.getDay() == 1, ''];
       }
     });
 
     // If this is not the first time the form has been submitted, use the last
     // selected value for the date.
-    if (PPR.attendanceForm.lastDate) {
-      $attendanceWeekField.val(PPR.attendanceForm.lastDate);
-    }
+    var lastDate = localStorage.getItem("formDate");
+    var lastFacility = localStorage.getItem("facilityName")
 
+    //Set Facility Field if stored in localstorage
+    if (lastFacility != null) {
+      console.log(lastFacility);
+      $facilityField.value = lastFacility;
+      $facilityField.val(lastFacility);
+      $facilityField.trigger("liszt:updated");
+      $facilityField.chosen().trigger("change");
+    };
+
+    //Set date if stored in localstorage
+    if (lastDate != null) {
+      console.log(lastDate);
+      $attendanceWeekField.val(lastDate);
+      
+    }
     // If this is the first time the form has been loaded, use the Monday
     // immediately preceding today.
     else {
@@ -61,11 +78,20 @@ window.KnackInit = function($) {
     // Remember the attendance form date whenever it changes.
     $attendanceWeekField.on('change', function() {
       PPR.attendanceForm.lastDate = $attendanceWeekField.val();
+      localStorage.setItem("formDate", PPR.attendanceForm.lastDate);
+    });
+
+    // Remember the facility name whenever it changes.
+     $facilityField.on('change', function() {
+      PPR.attendanceForm.facilityName = $facilityField.val();
+      localStorage.setItem("facilityName", PPR.attendanceForm.facilityName);
     });
 
     // Verify that numeric fields have a number in them
     $(numericFields.join(',')).each(function(i, el) {
       $(el).attr('pattern', '[0-9]*');
     });
+
+    return true;
   });
 };
